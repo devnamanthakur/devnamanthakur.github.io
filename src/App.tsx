7a2335e7729460a6c81776af72cadc019d35e71d
Naman Thakur <devnamanthakur853@gmail.com>
2026-03-18 18:04:19 +0530

    initial setup

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+
+function App(){
+  const [count, setCount] = useState(0)
+  return(
+    <div className='flex justify-center items-center font-bold text-center min-h-screen'>
+      <div>
+        
+      </div>
+    </div>
+  )
+}
+
+export default App
